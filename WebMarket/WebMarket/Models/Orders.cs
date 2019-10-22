b3b99cac4a1f5cc05ca2482c8240581e6d3af5a3
Hernantos <sattarov-artur@mail.ru>
2019-10-22 14:36:47 +0300

    Dobavili bd

---
+++ WebMarket/WebMarket/Models/Orders.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebMarket
+{
+    public partial class Orders
+    {
+        public Orders()
+        {
+            Ordersofusers = new HashSet<Ordersofusers>();
+        }
+
+        public int Id { get; set; }
+        public int? Product { get; set; }
+        public int? Owner { get; set; }
+        public int? Status { get; set; }
+        public int? Datecreated { get; set; }
+        public int? Delivery { get; set; }
+
+        public virtual Deliveries DeliveryNavigation { get; set; }
+        public virtual Users OwnerNavigation { get; set; }
+        public virtual Product ProductNavigation { get; set; }
+        public virtual Statuses StatusNavigation { get; set; }
+        public virtual ICollection<Ordersofusers> Ordersofusers { get; set; }
+    }
+}
---
+++ WebMarket/WebMarket/Models/Productimages.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebMarket
+{
+    public partial class Productimages
+    {
+        public int? Productid { get; set; }
+        public int? Imageid { get; set; }
+
+        public virtual Images Image { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
