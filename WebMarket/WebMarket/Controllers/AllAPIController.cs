4de45dc1a9c4426b60bd2120d0f02077cb4919b6
Egor <egor@getgain.ru>
2019-11-23 16:05:57 +0300

    change put method

--- WebMarket/WebMarket/Controllers/AllAPIController.cs
+++ WebMarket/WebMarket/Controllers/AllAPIController.cs
@@ -6,42 +6,42 @@ namespace WebMarket.Controllers
     [Route("/api")]
     public class AllAPIController : ControllerBase
     {
-        private TransientDependency _td;
-        private SingletonDependency _sd;
-        private ScopedDependency _scd;
-        public AllAPIController
-            (
-                TransientDependency td, 
-                SingletonDependency sd,
-                ScopedDependency scd
-            )
-        {
-            _td = td;
-            _sd = sd;
-            _scd = scd;
-        }
+        //private TransientDependency _td;
+        //private SingletonDependency _sd;
+        //private ScopedDependency _scd;
+        //public AllAPIController
+        //    (
+        //        TransientDependency td, 
+        //        SingletonDependency sd,
+        //        ScopedDependency scd
+        //    )
+        //{
+        //    _td = td;
+        //    _sd = sd;
+        //    _scd = scd;
+        //}
         [HttpGet]
         public string Get()
         {
-            
-//            var controllers =
-//                "https://webmarket911.herokuapp.com/api/categories/all  - все категории\n" +
-//                "https://webmarket911.herokuapp.com/api/categories/{id} - товары в категории\n" +
-//                "https://webmarket911.herokuapp.com/api/Others - не знаю что, надеюсь Артур пояснит\n" +
-//                "https://webmarket911.herokuapp.com/api/others/{id} - не знаю что, надеюсь Артур пояснит\n" +
-//                "https://webmarket911.herokuapp.com/api/others/novelties - новинки\n" +
-//                "https://webmarket911.herokuapp.com/api/others/Bestsellers - бестселлеры\n" +
-//                "https://webmarket911.herokuapp.com/api/productimages -какие то картинки к продуктам\n" +
-//                "https://webmarket911.herokuapp.com/api/productimages/{id} - какая то картинка к продукту\n" +
-//                "https://webmarket911.herokuapp.com/api/products/all - все продукты?\n" +
-//                "https://webmarket911.herokuapp.com/api/products/product/{id} - конкретный продукт\n" +
-//                "https://webmarket911.herokuapp.com/api/products/category/{id} - не знаю что здесь\n" +
-//                "https://webmarket911.herokuapp.com/api/profile  - put создание профиля/get все профили\n" +
-//                "https://webmarket911.herokuapp.com/api/profile/{id} - конкретный профиль\n" +
-//                "https://webmarket911.herokuapp.com/api/profile/auth - post возвращает true/false проверка логина и пароля при авторизации\n";
-//
-//            return controllers;
-            return _td.GetString() + " \t" + _sd.ToString() + " \t" + _scd.GetString();
+
+            var controllers =
+                "https://webmarket911.herokuapp.com/api/categories/all  - все категории\n" +
+                "https://webmarket911.herokuapp.com/api/categories/{id} - товары в категории\n" +
+                "https://webmarket911.herokuapp.com/api/Others - не знаю что, надеюсь Артур пояснит\n" +
+                "https://webmarket911.herokuapp.com/api/others/{id} - не знаю что, надеюсь Артур пояснит\n" +
+                "https://webmarket911.herokuapp.com/api/others/novelties - новинки\n" +
+                "https://webmarket911.herokuapp.com/api/others/Bestsellers - бестселлеры\n" +
+                "https://webmarket911.herokuapp.com/api/productimages -какие то картинки к продуктам\n" +
+                "https://webmarket911.herokuapp.com/api/productimages/{id} - какая то картинка к продукту\n" +
+                "https://webmarket911.herokuapp.com/api/products/all - все продукты?\n" +
+                "https://webmarket911.herokuapp.com/api/products/product/{id} - конкретный продукт\n" +
+                "https://webmarket911.herokuapp.com/api/products/category/{id} - не знаю что здесь\n" +
+                "https://webmarket911.herokuapp.com/api/profile  - put создание профиля/get все профили\n" +
+                "https://webmarket911.herokuapp.com/api/profile/{id} - конкретный профиль\n" +
+                "https://webmarket911.herokuapp.com/api/profile/auth - post возвращает true/false проверка логина и пароля при авторизации\n";
+
+            return controllers;
+            //return _td.GetString() + " \t" + _sd.ToString() + " \t" + _scd.GetString();
         }
     }
 
