ab8291b2a81b0254b4c79d137913ff033ea88c0f
Hernantos <sattarov-artur@mail.ru>
2019-11-24 14:05:11 +0300

    modified:   WebMarket/WebMarket/Controllers/CityController.cs 	modified:   WebMarket/WebMarket/Startup.cs

--- WebMarket/WebMarket/Startup.cs
+++ WebMarket/WebMarket/Startup.cs
@@ -20,7 +20,7 @@ namespace WebMarket
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<d6h4jeg5tcb9d8Context>(); //new
-<<<<<<< HEAD
+//<<<<<<< HEAD
             //services.AddControllers();
 
 
@@ -29,11 +29,11 @@ namespace WebMarket
             services.AddScoped<IScoped,ScopedDependency>();
 
 
-=======
+//=======
             //services.AddTransient<ITransientDependency,TransientDependency>();
             //services.AddSingleton<ISingletonDependency,SingletonDependency>();
             //services.AddScoped<IScopedDependency,ScopedDependency>();
->>>>>>> e5edf6bda4bd152bb873c328058491237197d3d6
+//>>>>>>> e5edf6bda4bd152bb873c328058491237197d3d6
             services.AddControllers();
         }
 
